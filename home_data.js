3a3d027a756429a18b6ed2df6f436a5d87d0cad1
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-05-19 19:39:19 +0530

    Initial Commit for file: home_data.js

---
+++ home_data.js
@@ -0,0 +1,178 @@
+export const item = {
+    "stories": [
+        {
+            "story": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/129.png",
+            "id": "422d89be-ab63-4578-bac0-d39fa8084824",
+            "story_label": "store"
+        },
+        {
+            "story": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/610.png",
+            "id": "9a8b20c6-9b43-4cef-a145-09c645bcf77e",
+            "story_label": "maintain"
+        },
+        {
+            "story": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/85.png",
+            "id": "e241c76a-5bae-40d3-a830-aab7c84de6c7",
+            "story_label": "budget"
+        }
+    ],
+    "id": "95c46da6-3b60-4cc6-aac0-fb8e13e16e51",
+    "feed": [
+        {
+            "post_actions": [
+                {
+                    "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/106.png",
+                    "id": "4bc89bde-58fa-4346-95ee-e8d433272638",
+                    "user_name": "like",
+                    "menu": true
+                },
+                {
+                    "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/722.png",
+                    "id": "3983b5e5-1324-4084-9870-8deec06ce323",
+                    "user_name": "hear",
+                    "menu": true
+                },
+                {
+                    "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/67.png",
+                    "id": "24145fe1-fa40-4094-9a77-3ebe6963ece7",
+                    "user_name": "five",
+                    "menu": true
+                }
+            ],
+            "id": "422d89be-ab63-4578-bac0-d39fa8084824",
+            "post_images": [
+                {
+                    "post": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/721.png",
+                    "id": "4bc89bde-58fa-4346-95ee-e8d433272638",
+                    "like": true,
+                    "comment": false,
+                    "share": true,
+                    "save": true
+                },
+                {
+                    "post": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/376.png",
+                    "id": "3983b5e5-1324-4084-9870-8deec06ce323",
+                    "like": true,
+                    "comment": true,
+                    "share": true,
+                    "save": false
+                },
+                {
+                    "post": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/75.png",
+                    "id": "24145fe1-fa40-4094-9a77-3ebe6963ece7",
+                    "like": true,
+                    "comment": false,
+                    "share": false,
+                    "save": true
+                }
+            ],
+            "post_label": "for",
+            "post_description": "Party whose reduce. Friend happen part interest than spring",
+            "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/559.png"
+        },
+        {
+            "post_actions": [
+                {
+                    "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/137.png",
+                    "id": "c69d88db-fc59-4ab7-8e53-8d089cb48826",
+                    "user_name": "power",
+                    "menu": true
+                },
+                {
+                    "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/214.png",
+                    "id": "66f4ca33-45d6-4157-b528-eb469169d2b2",
+                    "user_name": "wife",
+                    "menu": false
+                },
+                {
+                    "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/409.png",
+                    "id": "fe36c84b-a9e3-427a-851e-70398a94614a",
+                    "user_name": "as",
+                    "menu": true
+                }
+            ],
+            "id": "9a8b20c6-9b43-4cef-a145-09c645bcf77e",
+            "post_images": [
+                {
+                    "post": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/461.png",
+                    "id": "c69d88db-fc59-4ab7-8e53-8d089cb48826",
+                    "like": false,
+                    "comment": false,
+                    "share": true,
+                    "save": true
+                },
+                {
+                    "post": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/312.png",
+                    "id": "66f4ca33-45d6-4157-b528-eb469169d2b2",
+                    "like": false,
+                    "comment": false,
+                    "share": false,
+                    "save": true
+                },
+                {
+                    "post": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/134.png",
+                    "id": "fe36c84b-a9e3-427a-851e-70398a94614a",
+                    "like": false,
+                    "comment": false,
+                    "share": false,
+                    "save": true
+                }
+            ],
+            "post_label": "give",
+            "post_description": "Down land many sing international movie",
+            "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/655.png"
+        },
+        {
+            "post_actions": [
+                {
+                    "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/479.png",
+                    "id": "1d824d85-30ce-4539-9680-7b8a9b84f4a2",
+                    "user_name": "because",
+                    "menu": true
+                },
+                {
+                    "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/277.png",
+                    "id": "1bdafec2-a814-42c8-ab84-2e30d807e7d9",
+                    "user_name": "begin",
+                    "menu": false
+                },
+                {
+                    "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/118.png",
+                    "id": "1f5d4b20-fdc0-49cb-a054-3a65ff3ae93f",
+                    "user_name": "lawyer",
+                    "menu": false
+                }
+            ],
+            "id": "e241c76a-5bae-40d3-a830-aab7c84de6c7",
+            "post_images": [
+                {
+                    "post": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/126.png",
+                    "id": "1d824d85-30ce-4539-9680-7b8a9b84f4a2",
+                    "like": false,
+                    "comment": false,
+                    "share": false,
+                    "save": true
+                },
+                {
+                    "post": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/432.png",
+                    "id": "1bdafec2-a814-42c8-ab84-2e30d807e7d9",
+                    "like": false,
+                    "comment": false,
+                    "share": false,
+                    "save": false
+                },
+                {
+                    "post": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/730.png",
+                    "id": "1f5d4b20-fdc0-49cb-a054-3a65ff3ae93f",
+                    "like": false,
+                    "comment": false,
+                    "share": true,
+                    "save": true
+                }
+            ],
+            "post_label": "raise",
+            "post_description": "Western something prepare music. Decide",
+            "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/506.png"
+        }
+    ]
+}
